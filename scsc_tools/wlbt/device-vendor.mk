7ffa6b8651dcdd01ea19cdf392ebcb4738450524
Sam Protsenko <semen.protsenko@linaro.org>
2021-01-14 15:15:54 +0200

    Sam: Disable CNNTLogger
    In order to ease up the console freezing.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- scsc_tools/wlbt/device-vendor.mk
+++ scsc_tools/wlbt/device-vendor.mk
@@ -1,7 +1,7 @@
 ifeq ($(CONFIG_SAMSUNG_SCSC_WIFIBT),true)
 PRODUCT_PACKAGES_DEBUG += \
-        wland \
-        CNNTLogger
+        wland
+#  Removed:  CNNTLogger
 
 DEVICE_PACKAGE_OVERLAYS += \
                 vendor/samsung_slsi/scsc_tools/wlbt/device/samsung/overlay
